06fa2969afae99eea526f35da14ae0df7dc118c5
minahilirfan26 <duashaikh603@gmail.com>
2026-06-22 10:45:04 +0500

    class code

---
+++ class09/app.js
@@ -0,0 +1,69 @@
+// var trafficLight = prompt("enter the traffic light clor");  //red
+
+// if (trafficLight === "red") {
+//     alert("you must stop")
+// }
+// if(trafficLight === "yellow"){
+//     alert("ready to go")
+// }
+//  if (trafficLight === "green") {
+//     alert("please go")
+//  }
+
+
+// var age = "50";
+
+// if(age !== 50){
+//     console.log("you are not allow")
+// }else{
+//     console.log("you are allow")
+// }
+
+
+
+// var age = +prompt("enter your age"); 
+
+// var age = 50
+
+// if (age ==18) {
+//     console.log("adult")
+// }
+// if (age == 50){
+//     console.log("old")
+// }
+// if (age == 70){
+//     console.log("older")
+// }
+
+// if (age >= 15) {
+//     console.log("you r allow")
+// }
+// if (age >= 30) {
+//     console.log("you are not allow")
+// }
+
+// var trafficLight = prompt("enter the traffic light clor");
+
+// if(trafficLight === "red"){
+//     alert("you must stop")
+// } else if(trafficLight === "yellow"){
+//     alert("ready to go")
+// }else if(trafficLight === "green"){
+//     alert("please go")
+// }else{
+//     alert("please enter the correct traffic light colr")
+// }
+
+
+var city = prompt("please enter youe city"); //KARACHI
+// var city = KARACHI;
+if(city.toUpperCase() === "KARACHI"){
+    console.log("welcome to the city of lights")
+}else if(city.toLowerCase() === "lahore"){
+    console.log("welcome to the city of garden")
+}else if(city.toLowerCase() === "peshawar"){
+    console.log("welcome to the city of flower")
+}else{
+    console.log("sorry we can't find your city")
+}
+
