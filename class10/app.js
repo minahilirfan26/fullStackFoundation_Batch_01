0482523ed3357083b974eee6aa77dad32b246d0c
minahilirfan26 <duashaikh603@gmail.com>
2026-06-24 10:32:44 +0500

    class code
    Co-Authored-By: Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>

---
+++ class10/app.js
@@ -0,0 +1,43 @@
+// var age = +prompt("enter your age");
+// var city = prompt("enter your city");
+// var course = prompt("enter your course")
+
+// if(age >= 20 && city.toLowerCase() === "karachi" && course.toLowerCase === "Full Stack Foundation" ){
+//     console.log("you are enrolled")
+// }else{
+//     console.log("better luck next time");
+// }
+
+
+// if(age >= 20 || city.toLowerCase() === "karachi" || course.toLowerCase() === "fullstack"){
+//     console.log("you are enrolled")
+// }else{
+//     console.log("you are not enrolled")
+// }
+
+
+// var age = 25;
+// var city = "lahore";
+// var course = "web";
+
+
+// if(age >= 80 || city.toLowerCase() === "karachi" && course.toLowerCase() === "web"){
+//     console.log("you are enrolled")
+// }else{
+//     console.log("you are not enrolled")
+// }
+
+
+var frontend = prompt("enter your Frontend skills here");
+
+
+if(frontend === "react js"){
+    var backend = prompt("enter your backend skills here");
+    if(backend === "node js"){
+        console.log("kal sy office ajow")
+    }else{
+        console.log("phly njode js seekh k aow")
+    }
+}else{
+    console.log("ghar jow")
+}
