5c84d41f108ca8414109a0ee10c54652bb0241ab
minahilirfan26 <duashaikh603@gmail.com>
2026-07-02 10:36:26 +0500

    class code

---
+++ class12/app.js
@@ -0,0 +1,35 @@
+var fullStackFoundation = ["amna", "mahnoor", "urooj"];
+// fullStackFoundation[2] = "tayyaba";
+
+// console.log(fullStackFoundation[2]);
+
+// fullStackFoundation.push("neha", "nimrah")
+// console.log(fullStackFoundation.pop());
+// console.log(fullStackFoundation.length)
+
+// fullStackFoundation.unshift("Batch", 1);
+// fullStackFoundation.shift()
+// console.log(fullStackFoundation.shift());
+// var outPut = fullStackFoundation.slice(1,5)
+// console.log(outPut)
+
+
+// console.log(fullStackFoundation.splice(1,0, "tayyaba", "ayesha", "hira"))
+// console.log(fullStackFoundation)
+
+// var student = prompt("enter your name here");
+
+// if(!student){
+//     alert("please enter your name")
+  
+    
+// }else{
+//   alert("welcome to the class")
+// }
+
+// 0
+// -1
+// false
+// ""
+// NaN
+// null
